1d10d437c7729f9b7b0aa484d39e788daa27a5dd
M4th3ww <71942508+TeuzimMG@users.noreply.github.com>
2020-12-25 10:13:33 -0300

    Adição da opção _gamelichess

--- Lichess Bot.py
+++ Lichess Bot.py
@@ -1,15 +1,22 @@
 import discord
-import PIL
 import numpy as np
 import matplotlib
 from bs4 import BeautifulSoup
 import requests
 import json
+import chess
+import chess.svg
+from PIL import Image
+from io import BytesIO
 
-client = discord.Client()
+client = discord.Client() # Chama o client do discord
 
-@client.event
-async def on_message(message):
+@client.event # Determina um evento a ocorrer
+async def on_message(message): # 'on_message()' é usado para determinar oque fazer quando uma mensagem for mandada no servidor, o objeto 'message' recebe com ele alguns métodos..
+    #
+    # Está parte do código aqui em baixo eu não usei a API do LICHESS, então ficou muito confuso por isso nem a comentei
+    #
+
     rating_dict = {}
     partidas_dict = {}
     if message.content.startswith('_playerLichess'):
@@ -34,5 +41,35 @@ async def on_message(message):
         else:
             await message.channel.send('Este jogador não existe...')
 
+    #
+    # Até aqui eu não tinha conhecimento da existência de uma API para o LICHESS.
+    #
+
+    if message.content.startswith('_gamelichess'): # Está linha da inicio a pesquisa de jogos no Lichess
+        game_id = message.content.split(' ')[1] # O game_id recebe o código que vem depois da url do Liches
+        header = {'content-type': 'application/json'}
+        param = {'clocks':'false','evals':'false','litarate':'true'}
+        response = requests.get(f'https://lichess.org/game/export/{game_id}', params=param, headers=header) # Requisição na API do Lichess
+        if response.status_code == 200:
+            board = chess.Board()
+            print(response.text) 
+            info_lista = response.text.split('\n')
+            for info in info_list:
+                if '[Opening ' in info:
+                    abertura = info.replace('[Opening ','').replace(']','').replace('"','')
+                if '[Variant ' in info:
+                    variante = info.replace('[Variant ','').replace(']','').replace('"','')
+                if '[White ' in info:
+                    white = info.replace('[White ','').replace(']','').replace('"','')
+                if '[Black ' in info:
+                    black = info.replace('[Black ','').replace(']','').replace('"','')
+                if '1. ' in info:
+                    moves = info
+            embed=discord.Embed(title=f"{white} x {black}",description=f'{variante}', color=0x0) # Cria a mensagem que será enviado para o Discord com as informações
+            embed.add_field(name=f"Moves", value=f"{moves}", inline=True) # Cria a mensagem que será enviado para o Discord com as informações 
+            embed.add_field(name=f"Opening", value=f"{abertura}", inline=True) # Cria a mensagem que será enviado para o Discord com as informações
+            await message.channel.send(embed=embed) # Envia a Mensagem
+
 client.run("TOKEN")
+
 
